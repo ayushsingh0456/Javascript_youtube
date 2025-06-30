c3b018d85327d32165a48e12395d3caeffc0fc76
ayushsingh0456 <ayushsingh.06533@gmail.com>
2025-06-30 16:29:55 +0000

    add initial variable and datatype files with basic examples

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const studentId = 30001222134
+let name = "Ayush"
+var dob = "8th April 2005"
+universityname = "Makaut"
+let contact
+
+/*
+This is a comment
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.log(studentId)
+console.table([studentId,name, dob, universityname,contact])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"   // treat all the javascript code as a new vesion
+
+//alert(3+3)   // alert only works in browser, not nodejs
+
+let name= "Ayush"
+let age= 18
+let isadult=true
+let school=null
+let job
+
+//number => 2 to power 53
+//bigint
+//string=""
+//boolean => true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+console.log(typeof name)
+console.table([typeof name,typeof age, typeof isadult, typeof school, typeof school,typeof job])
+
+// type of null is always object
